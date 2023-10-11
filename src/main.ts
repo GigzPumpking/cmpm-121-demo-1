8b7044ad9dac7560cd109a108044d4ff3218209a
Gigz <gigzpumpking@gmail.com>
2023-10-11 15:39:07 -0700

    Step 1 complete, star emoji

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,7 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "Click me! &#11088;";
+app.append(button);
