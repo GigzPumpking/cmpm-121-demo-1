1a89789312230060feae6c7da399cdc42b8d0e8d
Gigz <gigzpumpking@gmail.com>
2023-10-11 15:47:08 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,14 @@ app.append(header);
 const button = document.createElement("button");
 button.innerHTML = "Click me! &#11088;";
 app.append(button);
+
+let counter: number = 0;
+
+const counterText = document.createElement("p");
+counterText.innerHTML = `Clicked ${counter} times!`;
+app.append(counterText);
+
+button.addEventListener("click", function () {
+  counter++;
+  counterText.innerHTML = `Clicked ${counter} times!`;
+});
