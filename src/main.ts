76336e62edf0408c1a4c83caa5e38ccd13413235
Gigz <gigzpumpking@gmail.com>
2023-10-18 04:07:42 -0700

    Step 6 complete, fixed counter incrementation to be based on player's fps rather than strictly 60 fps

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,13 @@ app.append(button);
 
 let counter: number = 0;
 let loopTime: number = 0;
-let loopFrequency: number = 60;
-let numUpgrades: number = 1;
+
+const generators: number[] = [0, 0, 0];
+const generatorLevels: number[] = [0, 0, 0];
+
+let lastTime = 0;
+let frameCount = 0;
+let fps = 0;
 
 // Buttons
 
@@ -32,33 +37,84 @@ button.addEventListener("click", function () {
   counterText.innerHTML = `Clicked ${counter} times!`;
 });
 
-const upgrade = document.createElement("button");
-upgrade.innerHTML = "Upgrade";
-app.append(upgrade);
+const upgrade1 = document.createElement("button");
+const upgrade1Text = document.createElement("p");
+createUpgrade(upgrade1, upgrade1Text, 10, 0, 1, `Generator 1: 10 stars`);
+initializeText(upgrade1Text, `${generators[1]} stars / sec`);
 
-upgrade.addEventListener("click", function () {
-  counter -= 10;
-  numUpgrades++;
-  counterText.innerHTML = `Clicked ${counter} times!`;
-  loopFrequency = 60 / numUpgrades;
-});
+const upgrade2 = document.createElement("button");
+const upgrade2Text = document.createElement("p");
+createUpgrade(upgrade2, upgrade2Text, 100, 1, 5, `Generator 2: 100 stars`);
+initializeText(upgrade2Text, `${generators[1]} stars / sec`);
 
-upgrade.disabled = true;
+const upgrade3 = document.createElement("button");
+const upgrade3Text = document.createElement("p");
+createUpgrade(upgrade3, upgrade3Text, 1000, 2, 50, `Generator 3: 1000 stars`);
+initializeText(upgrade3Text, `${generators[2]} stars / sec`);
 
-function loop() {
-  loopTime++;
-  if (counter >= 10) {
+function initializeText(text: HTMLParagraphElement, initalText: string) {
+  text.innerHTML = initalText;
+  app.append(text);
+}
+
+function createUpgrade(
+  upgrade: any,
+  upgradeText: any,
+  cost: number,
+  gen: number,
+  increase: number,
+  initialText: string,
+) {
+  upgrade.innerHTML = initialText;
+  app.append(upgrade);
+  upgrade.disabled = true;
+
+  upgrade.addEventListener("click", function () {
+    counter -= cost;
+    counterText.innerHTML = `Clicked ${counter} times!`;
+    generators[gen] += increase;
+    generatorLevels[gen]++;
+    upgrade.innerHTML = `Generator ${gen + 1}: ${cost} stars`;
+    upgradeText.innerHTML = `${generators[gen]} stars / sec (Level ${generatorLevels[gen]})`;
+  });
+}
+
+function buttonStatus(upgrade: any, cost: number) {
+  if (counter >= cost) {
     upgrade.disabled = false;
   } else {
     upgrade.disabled = true;
   }
+}
 
-  if (loopTime >= loopFrequency) {
+function calculateFPS(timestamp: number) {
+  const elapsed = timestamp - lastTime;
+
+  frameCount++;
+
+  if (elapsed >= 1000) {
+    fps = (frameCount * 1000) / elapsed;
+    frameCount = 0;
+    lastTime = timestamp;
+  }
+  requestAnimationFrame(calculateFPS);
+}
+
+function loop() {
+  loopTime++;
+  buttonStatus(upgrade1, 10);
+  buttonStatus(upgrade2, 100);
+  buttonStatus(upgrade3, 1000);
+
+  if (loopTime >= fps) {
     loopTime = 0;
-    counter++;
+    for (let i = 0; i < generators.length; i++) {
+      counter += generators[i];
+    }
     counterText.innerHTML = `Clicked ${counter} times!`;
   }
   window.requestAnimationFrame(loop);
 }
 
+window.requestAnimationFrame(calculateFPS);
 window.requestAnimationFrame(loop);
