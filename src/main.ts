768765e2ba0f4d96b0dabdce213304af63f284d0
Gigz <gigzpumpking@gmail.com>
2023-10-28 21:03:01 -0700

    Step 10 complete

--- src/main.ts
+++ src/main.ts
@@ -20,18 +20,60 @@ interface Star {
   rate: number;
   increase: number;
   level: number;
+  description: string;
 }
 
 const Stars: Star[] = [
-  { name: "Dwarf Star", cost: 10, rate: 0, increase: 0.1, level: 0 },
-  { name: "Giant Star", cost: 100, rate: 0, increase: 2, level: 0 },
-  { name: "Supergiant Star", cost: 1000, rate: 0, increase: 50, level: 0 },
+  {
+    name: "Subdwarf Star",
+    cost: 10,
+    rate: 0,
+    increase: 0.1,
+    level: 0,
+    description:
+      "The second smallest type of star, with a luminosity class of VI.",
+  },
+  {
+    name: "Dwarf Star",
+    cost: 50,
+    rate: 0,
+    increase: 0.5,
+    level: 0,
+    description:
+      "Otherwise known as a main-sequence star, with a luminosity class of V.",
+  },
+  {
+    name: "Giant Star",
+    cost: 100,
+    rate: 0,
+    increase: 2,
+    level: 0,
+    description:
+      "They lay above the main-sequence dwarf stars and have a luminosity class of III.",
+  },
+  {
+    name: "Supergiant Star",
+    cost: 1000,
+    rate: 0,
+    increase: 50,
+    level: 0,
+    description:
+      "Supergiants are the largest stars in the universe, with a luminosity class of Ib to Ia.",
+  },
+  {
+    name: "Hypergiant Star",
+    cost: 10000,
+    rate: 0,
+    increase: 1000,
+    level: 0,
+    description:
+      "Beyond comprehension, hypergiants are the largest stars in the universe, with a luminosity class of 0. One can only imagine their destructive demise into supernovae.",
+  },
 ];
 
 // Variables
 
 let counter: number = 0;
-
 let lastTime = 0;
 let frameCount = 0;
 let fps = 0;
@@ -47,20 +89,39 @@ button.addEventListener("click", function () {
   counterText.innerHTML = `Starage: ${counter.toFixed(2)} &#11088!`;
 });
 
+interface Upgrade {
+  button: HTMLButtonElement;
+  text: HTMLParagraphElement;
+  s: number;
+}
+
 const upgrade1 = document.createElement("button");
 const upgrade1Text = document.createElement("p");
-createUpgrade(upgrade1, upgrade1Text, 0);
-initializeText(upgrade1Text, `${Stars[0].rate} stars / sec`);
 
 const upgrade2 = document.createElement("button");
 const upgrade2Text = document.createElement("p");
-createUpgrade(upgrade2, upgrade2Text, 1);
-initializeText(upgrade2Text, `${Stars[1].rate} stars / sec`);
 
 const upgrade3 = document.createElement("button");
 const upgrade3Text = document.createElement("p");
-createUpgrade(upgrade3, upgrade3Text, 2);
-initializeText(upgrade3Text, `${Stars[2].rate} stars / sec`);
+
+const upgrade4 = document.createElement("button");
+const upgrade4Text = document.createElement("p");
+
+const upgrade5 = document.createElement("button");
+const upgrade5Text = document.createElement("p");
+
+const upgrades: Upgrade[] = [
+  { button: upgrade1, text: upgrade1Text, s: 0 },
+  { button: upgrade2, text: upgrade2Text, s: 1 },
+  { button: upgrade3, text: upgrade3Text, s: 2 },
+  { button: upgrade4, text: upgrade4Text, s: 3 },
+  { button: upgrade5, text: upgrade5Text, s: 4 },
+];
+
+upgrades.forEach((upgrade) => {
+  createUpgrade(upgrade.button, upgrade.text, upgrade.s);
+  initializeText(upgrade.text, `${Stars[upgrade.s].rate} stars / sec`);
+});
 
 function initializeText(text: HTMLParagraphElement, initalText: string) {
   text.innerHTML = initalText;
@@ -69,6 +130,7 @@ function initializeText(text: HTMLParagraphElement, initalText: string) {
 
 function createUpgrade(upgrade: any, upgradeText: any, s: number) {
   upgrade.innerHTML = `${Stars[s].name}: ${Stars[s].cost.toFixed(2)} stars`;
+  upgrade.title = Stars[s].description;
   app.append(upgrade);
   upgrade.disabled = true;
 
@@ -112,9 +174,9 @@ function round(num: number) {
 
 function loop() {
   calculateFPS(performance.now());
-  buttonStatus(upgrade1, Stars[0].cost);
-  buttonStatus(upgrade2, Stars[1].cost);
-  buttonStatus(upgrade3, Stars[2].cost);
+  upgrades.forEach((upgrade) => {
+    buttonStatus(upgrade.button, Stars[upgrade.s].cost);
+  });
 
   for (let i = 0; i < Stars.length; i++) {
     if (fps > 0) {
