c779d43aa7d5010fad32f0a8371cb52dc62ac140
Gigz <gigzpumpking@gmail.com>
2023-10-28 17:30:08 -0700

    Step 8 and 9 complete (this commit adds in unique upgrade names)

--- src/main.ts
+++ src/main.ts
@@ -14,13 +14,23 @@ const button = document.createElement("button");
 button.innerHTML = "Generate &#11088;";
 app.append(button);
 
+interface Star {
+  name: string;
+  cost: number;
+  rate: number;
+  increase: number;
+  level: number;
+}
+
+const Stars: Star[] = [
+  { name: "Dwarf Star", cost: 10, rate: 0, increase: 0.1, level: 0 },
+  { name: "Giant Star", cost: 100, rate: 0, increase: 2, level: 0 },
+  { name: "Supergiant Star", cost: 1000, rate: 0, increase: 50, level: 0 },
+];
+
 // Variables
 
 let counter: number = 0;
-
-const generators: number[] = [0, 0, 0];
-const generatorLevels: number[] = [0, 0, 0];
-const generatorCosts: number[] = [10, 100, 1000];
 
 let lastTime = 0;
 let frameCount = 0;
@@ -39,66 +49,39 @@ button.addEventListener("click", function () {
 
 const upgrade1 = document.createElement("button");
 const upgrade1Text = document.createElement("p");
-createUpgrade(
-  upgrade1,
-  upgrade1Text,
-  0,
-  0.1,
-  `Generator 1: ${generatorCosts[0].toFixed(2)} stars`,
-);
-initializeText(upgrade1Text, `${generators[1]} stars / sec`);
+createUpgrade(upgrade1, upgrade1Text, 0);
+initializeText(upgrade1Text, `${Stars[0].rate} stars / sec`);
 
 const upgrade2 = document.createElement("button");
 const upgrade2Text = document.createElement("p");
-createUpgrade(
-  upgrade2,
-  upgrade2Text,
-  1,
-  2,
-  `Generator 2: ${generatorCosts[1].toFixed(2)} stars`,
-);
-initializeText(upgrade2Text, `${generators[1]} stars / sec`);
+createUpgrade(upgrade2, upgrade2Text, 1);
+initializeText(upgrade2Text, `${Stars[1].rate} stars / sec`);
 
 const upgrade3 = document.createElement("button");
 const upgrade3Text = document.createElement("p");
-createUpgrade(
-  upgrade3,
-  upgrade3Text,
-  2,
-  50,
-  `Generator 3: ${generatorCosts[2].toFixed(2)} stars`,
-);
-initializeText(upgrade3Text, `${generators[2]} stars / sec`);
+createUpgrade(upgrade3, upgrade3Text, 2);
+initializeText(upgrade3Text, `${Stars[2].rate} stars / sec`);
 
 function initializeText(text: HTMLParagraphElement, initalText: string) {
   text.innerHTML = initalText;
   app.append(text);
 }
 
-function createUpgrade(
-  upgrade: any,
-  upgradeText: any,
-  gen: number,
-  increase: number,
-  initialText: string,
-) {
-  upgrade.innerHTML = initialText;
+function createUpgrade(upgrade: any, upgradeText: any, s: number) {
+  upgrade.innerHTML = `${Stars[s].name}: ${Stars[s].cost.toFixed(2)} stars`;
   app.append(upgrade);
   upgrade.disabled = true;
 
   upgrade.addEventListener("click", function () {
-    counter -= generatorCosts[gen];
-    generatorCosts[gen] = round(generatorCosts[gen] * 1.15);
-    upgrade.innerHTML = `Generator ${gen + 1}: ${generatorCosts[gen]} stars`;
-    counterText.innerHTML = `Clicked ${counter.toFixed(2)} times!`;
-    generators[gen] = generators[gen] + increase;
-    console.log(generators[gen]);
-    generatorLevels[gen]++;
-    upgrade.innerHTML = `Generator ${gen + 1}: ${generatorCosts[gen].toFixed(
-      2,
-    )} stars`;
-    upgradeText.innerHTML = `${generators[gen].toFixed(2)} stars / sec (Level ${
-      generatorLevels[gen]
+    counter -= Stars[s].cost;
+    Stars[s].cost = round(Stars[s].cost * 1.15);
+    upgrade.innerHTML = `${Stars[s].name}: ${Stars[s].cost} stars`;
+    counterText.innerHTML = `Starage: ${counter.toFixed(2)} &#11088!`;
+    Stars[s].rate += Stars[s].increase;
+    Stars[s].level++;
+    upgrade.innerHTML = `${Stars[s].name}: ${Stars[s].cost.toFixed(2)} stars`;
+    upgradeText.innerHTML = `${Stars[s].rate.toFixed(2)} stars / sec (Level ${
+      Stars[s].level
     })`;
   });
 }
@@ -129,13 +112,13 @@ function round(num: number) {
 
 function loop() {
   calculateFPS(performance.now());
-  buttonStatus(upgrade1, generatorCosts[0]);
-  buttonStatus(upgrade2, generatorCosts[1]);
-  buttonStatus(upgrade3, generatorCosts[2]);
+  buttonStatus(upgrade1, Stars[0].cost);
+  buttonStatus(upgrade2, Stars[1].cost);
+  buttonStatus(upgrade3, Stars[2].cost);
 
-  for (let i = 0; i < generators.length; i++) {
+  for (let i = 0; i < Stars.length; i++) {
     if (fps > 0) {
-      counter = counter + generators[i] / fps;
+      counter = counter + Stars[i].rate / fps;
     }
   }
   counterText.innerHTML = `Starage: ${counter.toFixed(2)} &#11088!`;
